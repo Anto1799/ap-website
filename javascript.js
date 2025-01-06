eb562fbe9b69f18de03ecadfc11acdf2d4ac477f
Antonio <antonioperrotta154@gmail.com>
2025-01-06 17:21:48 +0100

    EmailJS funzionante

---
+++ javascript.js
@@ -0,0 +1,22 @@
+(function() {
+    emailjs.init("1ZTZHKgqIvfywv9MT");
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            emailjs.sendForm('service_lk0anmq', 'template_rgu20ch', this)
+                .then(function(response) {
+                    console.log('SUCCESS!', response);
+                    alert('Message sent successfully!');
+                    form.reset();
+                }, function(error) {
+                    console.log('FAILED...', error);
+                    alert('Failed to send the message. Please try again.');
+                });
+        });
+    }
+});
